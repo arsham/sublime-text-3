c4fafe7cbc8e04385d1e81e4e7165fd7e0f99729
Arsham Shirvani <arshamshirvani@gmail.com>
2019-06-19 18:43:56 +0100

    Remove font cycler and add install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cd ../
+git clone git@github.com:tomv564/LSP.git 2> /dev/null
+git clone https://git.kuroku.io/GoSublime -b development 2> /dev/null
+git clone git@github.com:arsham/HaoGist.git 2> /dev/null
+git clone git@github.com:arsham/SublPlugs.git 2> /dev/null
